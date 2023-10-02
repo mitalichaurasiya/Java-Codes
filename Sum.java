575c60d1f0dc208425a2a49246173e2d08d375b8
Mitali Chaurasiya <71435717+mitalichaurasiya@users.noreply.github.com>
2023-10-02 22:17:23 +0530

    Create Sum.java

---
+++ Sum.java
@@ -0,0 +1,17 @@
+/* Write a program to sum three numbers in Java */
+
+//Code
+import java.util.*;
+public class Sum{
+    public static void main(String args[]){
+        Scanner maww=new Scanner(System.in);
+        int a=maww.nextInt();
+        int b=maww.nextInt();
+        int c=maww.nextInt();
+        int x=a+b+c;
+        System.out.println("sum of three numbers: "+x);
+
+
+
+    }
+}
