84f2491cd10f24954b15fe49f9c64eda8f5b71d7
Mitali Chaurasiya <71435717+mitalichaurasiya@users.noreply.github.com>
2023-10-02 22:28:07 +0530

    Create Kmtomiles.java

---
+++ Kmtomiles.java
@@ -0,0 +1,14 @@
+/* Write a Java program to convert Kilometers to miles. */
+
+//Code:
+import java.util.Scanner;
+
+public class Kmtomiles {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter the Kilometers: ");
+        float km=sc.nextFloat();
+        float miles=(float) (km/1.609);
+        System.out.println(km+" yskm into miles will be "+miles+" miles");
+    }
+}
