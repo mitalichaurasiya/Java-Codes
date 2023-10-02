1ea8ef3e2e4f93299e38e39881f8c400ae413caf
Mitali Chaurasiya <71435717+mitalichaurasiya@users.noreply.github.com>
2023-10-02 22:39:22 +0530

    Create expression_Calculator.java

---
+++ expression_Calculator.java
@@ -0,0 +1,12 @@
+/*What will be the result of the following expression:
+
+float a = 7/4 * 9/2*/
+
+//code
+
+public class expression_Calculator {
+    public static void main(String[] args) {
+        float a= (7/4.0f * 9/2.0f);
+        System.out.println(a);
+    }
+}
